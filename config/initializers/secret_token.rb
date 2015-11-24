957a882378d071e02445973a69a24ea175d03af5
anta-tinypulse <anta@tinypulse.com>
2015-11-25 01:10:52 +0700

    update .gitignore

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '7ad98e4538e418ab562214bd317ea706c73c221b6560713078cd7d4c07f1a0dca9817f1ff95a9963654e4fd9a92f08025ada52e0639ae29f658738ad0e73363f'
